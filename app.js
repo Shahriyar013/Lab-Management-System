aa14fe6271bb083904e7945371533ceabef2a3af
Tanvir91221 <t6064773@gmail.com>
2026-08-15 14:47:22 +0600

    Added admin functions

--- app.js
+++ app.js
@@ -139,6 +139,25 @@ function studentMenu() {
     });
 }
 
+// Admin Login
+function adminLogin() {
+
+    console.log("\n=== ADMIN LOGIN ===");
+
+    rl.question("Username: ", username => {
+        rl.question("Password: ", password => {
+
+            if (username === "Admin" && password === "1234") {
+                adminModule.adminMenu();
+            } else {
+                console.log("Invalid admin login!");
+                start();
+            }
+        });
+    });
+}
+
+
 
 //SUBMIT COMPLAINT
 
@@ -267,8 +286,175 @@ function myComplaints() {
     });
 }
 
+function adminMenu() {
+
+    console.log("\n=== ADMIN PANEL ===");
+    console.log("1. View All Complaints");
+    console.log("2. View Pending Complaints");
+    console.log("3. Update Complaint");
+    console.log("4. Logout");
+    console.log("5. Exit");
+
+    rl.question("Choice: ", (choice) => {
+
+        switch (choice.trim()) {
+
+            case "1":
+                viewAll();
+                break;
+
+            case "2":
+                viewPending();
+                break;
+
+            case "3":
+                updateComplaint();
+                break;
+
+            case "4":
+                start();
+                break;
+
+            case "5":
+                rl.close();
+                break;
+
+            default:
+                console.log("Invalid choice!");
+                adminMenu();
+        }
+    });
+}
 
 
-// ===== START PROGRAM =====
+
+
+
+
+// ===== ALL COMPLAINTS =====
+
+function viewAll() {
+
+    const complaints = read(COMPLAINT_FILE);
+
+    console.log("\n=== ALL COMPLAINTS ===");
+
+    if (complaints.length === 0) {
+        console.log("No complaints.");
+    } else {
+        complaints.forEach(showComplaint);
+    }
+
+    rl.question("\nPress Enter to continue...", () => {
+        adminMenu();
+    });
+}
+
+function showComplaint(c) {
+
+    console.log("-----------------------");
+    console.log("ID:", c.id);
+    console.log("Student:", c.studentId);
+    console.log("Room:", c.room);
+  
+    
+    console.log("Problem:", c.problem);
+    console.log("Priority:", c.priority);
+    console.log("Status:", c.status);
+}
+
+
+// ===== PENDING COMPLAINTS =====
+
+function viewPending() {
+
+    const complaints = read(COMPLAINT_FILE).filter(
+        c => c.status === "Pending"
+    );
+
+    console.log("\n=== PENDING COMPLAINTS ===");
+
+    if (complaints.length === 0) {
+        console.log("No pending complaints.");
+    } else {
+        complaints.forEach(showComplaint);
+    }
+
+    rl.question("\nPress Enter to continue...", () => {
+        adminMenu();
+    });
+}
+
+// ===== UPDATE COMPLAINT =====
+
+function updateComplaint() {
+
+    const complaints = read(COMPLAINT_FILE);
+
+    console.log("\n=== UPDATE COMPLAINT ===");
+
+    rl.question("Enter Complaint ID: ", (id) => {
+
+        id = id.trim();
+
+        const complaint = complaints.find(
+            c => c.id.toUpperCase() === id.toUpperCase()
+        );
+
+        if (!complaint) {
+            console.log("Complaint not found!");
+            return adminMenu();
+        }
+
+        console.log("\nCurrent Status:", complaint.status);
+
+        console.log("\n1. Pending");
+        console.log("2. In Progress");
+        console.log("3. Solved");
+
+        rl.question("New Status: ", (choice) => {
+
+            choice = choice.trim();
+
+            let status;
+
+            if (choice === "1") {
+                status = "Pending";
+            }
+            else if (choice === "2") {
+                status = "In Progress";
+            }
+            else if (choice === "3") {
+                status = "Solved";
+            }
+            else {
+                console.log("Invalid choice!");
+                return adminMenu();
+            }
+
+            complaint.status = status;
+
+            save(COMPLAINT_FILE, complaints);
+
+            console.log(
+                "\nComplaint updated successfully!"
+            );
+
+            console.log(
+                "New Status:",
+                complaint.status
+            );
+
+            rl.question(
+                "\nPress Enter to continue...",
+                () => {
+                    adminMenu();
+                }
+            );
+        });
+    });
+}
+
+//  START PROGRAM 
 
 start();
